5bc5e89cc2587a6089874ac6808455f9b1cf2c7c
Kumar Gaurav <kumar24.gaurav@ril.com>
2022-11-28 14:19:00 +0530

    Implementing basic game loop

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('Hello in the Snake World');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 600;
+var x = 0;
+function update() {
+    x++;
+}
+function draw() {
+    context.fillRect(x, 200, 40, 40);
+    context.fill();
+}
+(function gameLoop() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    update();
+    draw();
+    window.requestAnimationFrame(gameLoop);
+})();
